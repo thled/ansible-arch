38812b550aa0f490623198da49db136990c237fa
Thomas Le Duc <dev@tleduc.de>
2021-03-08 13:22:19 +0100

    Add battery status to bar

--- roles/arch/files/leftwm/themes/current/xmobar-config.hs
+++ roles/arch/files/leftwm/themes/current/xmobar-config.hs
@@ -8,7 +8,7 @@ Config { font = "-misc-fixed-*-*-*-*-13-*-*-*-*-*-*-*"
     , allDesktops = True
     , pickBroadest = False
     , overrideRedirect = False
-    , commands = [    Run Weather "EDDL" ["-t"," <tempC>C","-L","64","-H","77","--normal","green","--high","red","--low","lightblue"] 36000
+    , commands = [    Run Weather "EDDL" ["-t"," <tempC>C","-L","15","-H","25","--normal","green","--high","red","--low","lightblue"] 36000
                     , Run Network "eno1" ["-L","0","-H","32","--normal","green","--high","red"] 100
                     , Run Wireless "" [ "-t", "<quality>" ] 100
                     , Run MultiCpu ["-L","15","-H","50","--normal","green","--high","red"] 50
@@ -17,8 +17,18 @@ Config { font = "-misc-fixed-*-*-*-*-13-*-*-*-*-*-*-*"
                     , Run TopProc [] 50
                     , Run Date "%a %b %_d %Y %H:%M" "date" 50
                     , Run UnsafeStdinReader
+                    , Run Battery [ "--template" , "Batt: <acstatus>"
+                             , "--Low"      , "20"
+                             , "--High"     , "80"
+                             , "--low"      , "darkred"
+                             , "--normal"   , "darkorange"
+                             , "--"
+                                       , "-o"	, "<left>% (<timeleft>)"
+                                       , "-O"	, "Charging"
+                                       , "-i"	, "Charged"
+                             ] 50
     ]
     , sepChar = "%"
     , alignSep = "}{"
-    , template = "%UnsafeStdinReader% }{ %multicpu% | %memory% * %swap% | %eno1% * %wi% | %date% | %EDDL%"
+    , template = "%UnsafeStdinReader% }{ %multicpu% | %memory% * %swap% | %eno1% * %wi% | %battery% | %date% | %EDDL%"
 }
