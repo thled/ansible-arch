0a6d371c3c51d27db904bcf2f058d7f89a2452ce
Thomas Le Duc <dev@tleduc.de>
2021-04-30 13:34:01 +0200

    Add XMobar for XMonad

--- roles/arch/files/xmonad/xmonad.hs
+++ roles/arch/files/xmonad/xmonad.hs
@@ -1,7 +1,22 @@
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run
+import XMonad.Util.SpawnOnce
 
-main = xmonad defaultConfig
-        { modMask = mod4Mask -- Use Super instead of Alt
+main = do
+    xmproc <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
+    xmonad $ docks def
+        { modMask = mod4Mask
         , terminal = "alacritty"
-        -- more changes
+        , focusedBorderColor = "#0066FF"
+        , layoutHook=avoidStruts $ layoutHook def
+        , logHook = dynamicLogWithPP xmobarPP
+            { ppOutput = hPutStrLn xmproc
+            , ppCurrent = xmobarColor "black" "grey" . wrap "[" "]"
+            , ppHidden = xmobarColor "grey" "black" . wrap "(" ")"
+            , ppHiddenNoWindows = xmobarColor "grey" "black"
+            , ppSep = " | "
+            , ppTitle = xmobarColor "#00AAFF" "" . shorten 100
+            }
         }
